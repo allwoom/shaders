035f324f8e58cd4169715d124a437ac60cfbf299
Michael Allwood <mikeallwood@hotmail.com>
2019-06-21 03:02:55 -0400

    Passing in parameters and rendering colours

--- starfield/shaderHelper.js
+++ starfield/shaderHelper.js
@@ -1,58 +1,73 @@
 
-		/**
-		 * Creates and compiles a shader.
-		 *
-		 * @param {!WebGLRenderingContext} gl The WebGL Context.
-		 * @param {string} shaderSource The GLSL source code for the shader.
-		 * @param {number} shaderType The type of shader, VERTEX_SHADER or
-		 *     FRAGMENT_SHADER.
-		 * @return {!WebGLShader} The shader.
-		 */
-		function compileShader(gl, shaderSource, shaderType) {
-            // Create the shader object
-            var shader = gl.createShader(shaderType);
-           
-            // Set the shader source code.
-            gl.shaderSource(shader, shaderSource);
-           
-            // Compile the shader
-            gl.compileShader(shader);
-           
-            // Check if it compiled
-            var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-            if (!success) {
-              // Something went wrong during compilation; get the error
-              throw "could not compile shader:" + gl.getShaderInfoLog(shader);
-            }
-           
-            return shader;
-          }
-
-          /**
- * Creates a program from 2 shaders.
+/**
+ * Creates and compiles a shader.
  *
- * @param {!WebGLRenderingContext) gl The WebGL context.
- * @param {!WebGLShader[]} shaders
- * @return {!WebGLProgram} A program.
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} shaderSource The GLSL source code for the shader.
+ * @param {number} shaderType The type of shader, VERTEX_SHADER or
+ *     FRAGMENT_SHADER.
+ * @return {!WebGLShader} The shader.
  */
+function compileShader(gl, shaderSource, shaderType) {
+  // Create the shader object
+  var shader = gl.createShader(shaderType);
+
+  // Set the shader source code.
+  gl.shaderSource(shader, shaderSource);
+
+  // Compile the shader
+  gl.compileShader(shader);
+
+  // Check if it compiled
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (!success) {
+    // Something went wrong during compilation; get the error
+    throw "could not compile shader:" + gl.getShaderInfoLog(shader);
+  }
+
+  return shader;
+}
+
+
+/**
+* Creates a program from 2 shaders.
+*
+* @param {!WebGLRenderingContext) gl The WebGL context.
+* @param {!WebGLShader[]} shaders
+* @return {!WebGLProgram} A program.
+*/
 function createProgram(gl, shaders) {
   // create a program.
   var program = gl.createProgram();
- 
+
   // attach the shaders.
-  for(var s in shaders){
-      gl.attachShader(program, shaders[s]);
+  for (var s in shaders) {
+    gl.attachShader(program, shaders[s]);
   }
-  
+
   // link the program.
   gl.linkProgram(program);
- 
+
   // Check if it linked.
   var success = gl.getProgramParameter(program, gl.LINK_STATUS);
   if (!success) {
-      // something went wrong with the link
-      throw ("program filed to link:" + gl.getProgramInfoLog (program));
+    // something went wrong with the link
+    throw ("program filed to link:" + gl.getProgramInfoLog(program));
   }
- 
+
   return program;
 };
+
+/**
+* Map a value in one range to a value in another range
+* @param {Number} value 
+* @param {Number} min1 
+* @param {Number} max1 
+* @param {Number} min2 
+* @param {Number} max2 
+*/
+function mapValue(value, min1, max1, min2, max2) {
+  var percent = (value - min1) / (max1 - min1);
+
+  return percent * (max2 - min2) + min2;
+}
---
+++ starfield/starfield.js
@@ -0,0 +1,89 @@
+var _gl = null;
+var _drawBuffer = null;
+var _vertexShader = null;
+var _fragmentShader = null;
+var _program = null;
+
+// For updating time
+var _timeLoc = null;
+var _elapsedTime = 0;
+
+var _resolutionLoc = null;
+
+function onLoad() {
+    initProgram();
+
+    if (_gl) {
+        requestAnimationFrame(render);
+    }
+}
+
+function onWindowResize(){
+    // update resolution if relevant
+}
+
+function initProgram() {
+    const canvas = document.querySelector("#glCanvas");
+
+    // Initialize the GL context
+    _gl = canvas.getContext("webgl");
+
+    // Only continue if WebGL is available and working
+    if (_gl === null) {
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+        return;
+    }
+
+    var vertexShaderSource = document.getElementById("vertexShader").text;
+    _vertexShader = compileShader(_gl, vertexShaderSource, _gl.VERTEX_SHADER);
+
+    var fragmentShaderSource = document.getElementById("fragmentShader").text;
+    _fragmentShader = compileShader(_gl, fragmentShaderSource, _gl.FRAGMENT_SHADER);
+
+    _program = createProgram(_gl, [_vertexShader, _fragmentShader]);
+
+    _drawBuffer = _gl.createBuffer();
+    _gl.bindBuffer(_gl.ARRAY_BUFFER, _drawBuffer);
+    _gl.bufferData(
+        _gl.ARRAY_BUFFER,
+        new Float32Array([
+            -1.0, -1.0,
+            1.0, -1.0,
+            -1.0, 1.0,
+            -1.0, 1.0,
+            1.0, -1.0,
+            1.0, 1.0]),
+        _gl.STATIC_DRAW
+    );
+
+    // Draw with our program active
+    _gl.useProgram(_program);
+
+    // Find and intialize shader uniforms
+    _timeLoc = _gl.getUniformLocation(_program, "time");
+    _resolutionLoc = _gl.getUniformLocation(_program, "resolution");
+
+    _gl.uniform1f(_timeLoc, _elapsedTime);
+    _gl.uniform2fv(_resolutionLoc, [canvas.clientWidth, canvas.clientHeight]);
+}
+
+function render(time) {    
+    // Update shader variable(s)
+    _gl.uniform1f(_timeLoc, _elapsedTime += 1.0 / 60.0);
+
+    // Set clear color to black, fully opaque
+    _gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    _gl.clear(_gl.COLOR_BUFFER_BIT);
+
+    var positionLocation = _gl.getAttribLocation(_program, "a_position");
+    _gl.enableVertexAttribArray(positionLocation);
+    _gl.vertexAttribPointer(positionLocation, 2, _gl.FLOAT, false, 0, 0);
+    _gl.drawArrays(_gl.TRIANGLES, 0, 6);
+
+    requestAnimationFrame(render);
+}
+
+
+window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener( 'load', onLoad);
